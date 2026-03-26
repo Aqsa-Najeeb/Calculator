8c396fcd13ce1b1a556b862f3ee292fbc732da39
Aqsa Najeeb <jenscar777@gmail.com>
2026-03-26 18:59:02 +0500

    Reduced repetition in calculator-script.js and updated button tags in Calculator.html

--- calculator-script.js
+++ calculator-script.js
@@ -1,351 +1,100 @@
-let a=0, b=0, operator=0;
-		
-			
-			//CLICKING BUTTONS
-			
-			document.getElementById("one").addEventListener("click", function() 
+let a = "";
+			let b = "";
+			let operator = null;
+
+			const display = document.getElementById("ansDisplay");
+
+			function updateDisplay(value) 
 			{
-				if(operator==0)
-				{
-					if(a==0)
-					{
-						a = 1;
-					}
-					else
-					{
-						a = a + "1";
-					}
-					document.getElementById("ansDisplay").innerHTML = a;
-				}
-				else
-				{
-					if(b==0)
-					{
-						b = 1;
-					}
-					else
-					{
-						b = b + "1";
-					}
-					document.getElementById("ansDisplay").innerHTML = b;
-				}
-			});
-			
-			document.getElementById("two").addEventListener("click", function() 
+				display.innerHTML = value || 0;
+			}
+
+			// Number input
+			function numberInput(num) 
 			{
-				if(operator==0)
+				if (operator === null) 
 				{
-					if(a==0)
-					{
-						a = 2;
-					}
-					else
-					{
-						a = a + "2";
-					}
-					document.getElementById("ansDisplay").innerHTML = a;
-				}
-				else
+					a += num; //concatenates
+					updateDisplay(a);
+				} 
+				else 
 				{
-					if(b==0)
-					{
-						b = 2;
-					}
-					else
-					{
-						b = b + "2";
-					}
-					document.getElementById("ansDisplay").innerHTML = b;
+					b += num;
+					updateDisplay(b);
 				}
-			});
-			
-			document.getElementById("three").addEventListener("click", function() 
+			}
+
+			// Operator inputs
+			function operatorInput(op) 
 			{
-				if(operator==0)
-				{
-					if(a==0)
-					{
-						a = 3;
-					}
-					else
-					{
-						a = a + "3";
-					}
-					document.getElementById("ansDisplay").innerHTML = a;
-				}
-				else
-				{
-					if(b==0)
-					{
-						b = 3;
-					}
-					else
-					{
-						b = b + "3";
-					}
-					document.getElementById("ansDisplay").innerHTML = b;
-				}
-			});
-			
-			document.getElementById("four").addEventListener("click", function() 
-			{
-				if(operator==0)
-				{
-					if(a==0)
-					{
-						a = 4;
-					}
-					else
-					{
-						a = a + "4";
-					}
-					document.getElementById("ansDisplay").innerHTML = a;
-				}
-				else
-				{
-					if(b==0)
-					{
-						b = 4;
-					}
-					else
-					{
-						b = b + "4";
-					}
-					document.getElementById("ansDisplay").innerHTML = b;
-				}
-			});
-			
-			document.getElementById("five").addEventListener("click", function() 
-			{
-				if(operator==0)
-				{
-					if(a==0)
-					{
-						a = 5;
-					}
-					else
-					{
-						a = a + "5";
-					}
-					document.getElementById("ansDisplay").innerHTML = a;
-				}
-				else
-				{
-					if(b==0)
-					{
-						b = 5;
-					}
-					else
-					{
-						b = b + "5";
-					}
-					document.getElementById("ansDisplay").innerHTML = b;
-				}
-			});
-			
-			document.getElementById("six").addEventListener("click", function() 
-			{
-				if(operator==0)
-				{
-					if(a==0)
-					{
-						a = 6;
-					}
-					else
-					{
-						a = a + "6";
-					}
-					document.getElementById("ansDisplay").innerHTML = a;
-				}
-				else
-				{
-					if(b==0)
-					{
-						b = 6;
-					}
-					else
-					{
-						b = b + "6";
-					}
-					document.getElementById("ansDisplay").innerHTML = b;
-				}
-			});
-			
-			document.getElementById("seven").addEventListener("click", function() 
-			{
-				if(operator==0)
-				{
-					if(a==0)
-					{
-						a = 7;
-					}
-					else
-					{
-						a = a + "7";
-					}
-					document.getElementById("ansDisplay").innerHTML = a;
-				}
-				else
-				{
-					if(b==0)
-					{
-						b = 7;
-					}
-					else
-					{
-						b = b + "7";
-					}
-					document.getElementById("ansDisplay").innerHTML = b;
-				}
-			});
-			
-			document.getElementById("eight").addEventListener("click", function() 
-			{
-				if(operator==0)
-				{
-					if(a==0)
-					{
-						a = 8;
-					}
-					else
-					{
-						a = a + "8";
-					}
-					document.getElementById("ansDisplay").innerHTML = a;
-				}
-				else
-				{
-					if(b==0)
-					{
-						b = 8;
-					}
-					else
-					{
-						b = b + "8";
-					}
-					document.getElementById("ansDisplay").innerHTML = b;
-				}
-			});
-			
-			document.getElementById("nine").addEventListener("click", function() 
-			{
-				if(operator==0)
-				{
-					if(a==0)
-					{
-						a = 9;
-					}
-					else
-					{
-						a = a + "9";
-					}
-					document.getElementById("ansDisplay").innerHTML = a;
-				}
-				else
-				{
-					if(b==0)
-					{
-						b = 9;
-					}
-					else
-					{
-						b = b + "9";
-					}
-					document.getElementById("ansDisplay").innerHTML = b;
-				}
-			});
-			
-			document.getElementById("zero").addEventListener("click", function() 
-			{
-				if(operator==0)
-					{
-						if(a==0)
-						{
-							a = 0;
-						}
-						else
-						{
-							a = a + "0";
-						}
-						document.getElementById("ansDisplay").innerHTML = a;
-					}
-					else
-					{
-						if(b==0)
-						{
-							b = 0;
-						}
-						else
-						{
-							b = b + "0";
-						}
-						document.getElementById("ansDisplay").innerHTML = b;
-					}
-			});
-			
-			document.getElementById("plus").addEventListener("click", function() 
-			{
-				operator = 1;
-				document.getElementById("ansDisplay").innerHTML = "+";
-			});
-			
-			document.getElementById("minus").addEventListener("click", function() 
-			{
-				operator = 2;
-				document.getElementById("ansDisplay").innerHTML = "-";
-			});
-			
-			document.getElementById("ster").addEventListener("click", function() 
-			{
-				operator = 3;
-				document.getElementById("ansDisplay").innerHTML = "*";
-			});
-			
-			document.getElementById("slash").addEventListener("click", function() 
-			{
-				operator = 4;
-				document.getElementById("ansDisplay").innerHTML = "/";
-			});
-			
-			
-			
-			document.getElementById("equal").addEventListener("click", function() 
-			{
-				let numA = parseInt(a);
-				let numB = parseInt(b);
-				let result;
+				if (a === "") 
+				return;
 				
-				
-				if(operator==1)
-				{
-					result = numA+numB;
-				}
-				else if(operator==2)
-				{
-					result = numA-numB;
-				}
-				else if(operator==3)
-				{
-					result = numA*numB;
-				}
-				else if(operator==4)
-				{
-					result = numA/numB;
-				}
-				
-				document.getElementById("ansDisplay").innerHTML = result;
-				
-				operator = 0;
-				a = 0;
-				b = 0;
-			});
-			
-			
-			document.getElementById("c").addEventListener("click", function() 
+				operator = op;
+				updateDisplay(op);
+			}
+
+			// Calculate Result
+			function calculate() 
 			{
-				operator = 0;
-				a = 0;
-				b = 0;
-				document.getElementById("ansDisplay").innerHTML = 0;
-				
+				let numA = parseFloat(a);
+				let numB = parseFloat(b);
+				let result = 0;
+
+				if (operator === "+")
+				{ 
+					result = numA + numB;
+				}
+				else if (operator === "-") 
+				{
+					result = numA - numB;
+				}
+				else if (operator === "*") 
+				{
+					result = numA * numB;
+				}
+				else if (operator === "/") 
+				{
+					if (numB === 0) {
+						updateDisplay("Error");
+						return;
+					}
+					result = numA / numB;
+				}
+
+				updateDisplay(result);
+
+				a = result.toString();
+				b = "";
+				operator = null;
+			}
+
+			function clear() 
+			{
+				a = "";
+				b = "";
+				operator = null;
+				updateDisplay(0);
+			}
+
+			// digits
+			document.querySelectorAll(".num").forEach(btn => 
+			{
+				btn.addEventListener("click", () => 
+				{
+					numberInput(btn.dataset.value);
+				});
 			});
+
+			/// operators
+			document.querySelectorAll(".op").forEach(btn => 
+			{
+				btn.addEventListener("click", () => 
+				{
+					operatorInput(btn.dataset.value);
+				});
+			});
+
+
+			document.getElementById("equal").onclick = calculate;
+			document.getElementById("c").onclick = clear;
